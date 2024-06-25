aac3a43842b7fef7e1f5a89f8ae284c8b4937f63
chaeyoo <chaeh0124@gmail.com>
2024-06-25 21:48:04 +0900

    [FEAT] 포스팅 서비스 - auth0 인증 토큰 - eslint 설정 - 포스팅 목록, 삭제, 등록 api/컴포넌트 추가

--- apps/shell/src/bootstrap.tsx
+++ apps/shell/src/bootstrap.tsx
@@ -2,5 +2,6 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "@my-career/ui-kit/index.css";
+import "./index.css";
 
 createRoot(document.getElementById("app")!).render(<App />);
