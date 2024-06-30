31b3634016e7dd11c5f29c7ecf4926713ae27d41
chaeyoo <chaeh0124@gmail.com>
2024-07-01 08:11:01 +0900

    [FEAT] edu 페이지 작업 - 프로필 - 강의 목록, 상세

---
+++ apps/edu/src/pages/page-list.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import { coursesAtom } from "../atoms";
+import { useAtomValue } from "jotai";
+import CourseListItem from "../components/course-list-item";
+
+const PageList: React.FC = () => {
+	const courses = useAtomValue(coursesAtom);
+
+	return (
+		<>
+			{courses.map((course) => (
+				<CourseListItem key={course.id} {...course} />
+			))}
+		</>
+	);
+};
+
+export default PageList;
